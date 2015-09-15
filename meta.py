36dcb3dfc2a5a1475879b7d1383ea0149586d499
Tony Asleson <tasleson@redhat.com>
2015-09-15 18:03:23 -0500

    Improve parser perf
    Using static methods in a class yields slower code, switching to
    functions.
    
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- meta.py
+++ meta.py
@@ -18,13 +18,182 @@ import string
 import socket
 
 
+_IDENT_CH = string.ascii_letters + string.digits + '_.-'
+_WHITESPACE_CH = ' \t\n\r'
+_NUMBER_CH = string.digits + '.'
+
+
+def _ws(data, i):
+    while data[i] in _WHITESPACE_CH:
+        i += 1
+    return i
+
+
+def _comment(data, i):
+    if data[i] != '#':
+        raise Exception(
+            """Comment expecting'#' not %s, pos= %d, data= %s""" %
+            (data[i], i, data[i:i + 100]))
+
+    while data[i] != "\n":
+        i += 1
+    return i + 1        # Chomp new '\n'
+
+
+def _get_identifier(data, i):
+    st = i
+    while data[i] in _IDENT_CH:
+        i += 1
+    return data[st:i], i
+
+
+def _get_number(data, i):
+    rc = None
+    st = i
+
+    while data[i] in _NUMBER_CH:
+        i += 1
+
+    raw = data[st:i]
+
+    if '.' in raw:
+        rc = float(raw)
+    else:
+        rc = long(raw)
+
+    return rc, i
+
+
+def _get_string(data, i):
+
+    if data[i] != '"':
+        raise Exception(
+            """String expecting '"' not %s, pos= %d, data= %s""" %
+            (data[i], i, data[i:i + 100]))
+    i += 1
+    st = i
+
+    while data[i] != '"':
+        i += 1
+    return data[st:i], i + 1            # Chomp the ending '"'
+
+
+def _get_array(data, i):
+    rc = []
+
+    if data[i] != '[':
+        raise Exception("Array expecting '[' not %s, pos= %d, data= %s" %
+                        (data[i], i, data[i:i + 100]))
+
+    i += 1
+
+    while True:
+        ch = data[i]
+
+        if ch in _WHITESPACE_CH:
+            i = _ws(data, i)
+        elif ch == '#':
+            i = _comment(data, i)
+        else:
+            # We should only be processing, numbers or strings
+            if ch == '"':
+                value, i = _get_string(data, i)
+                rc.append(value)
+            elif ch in _NUMBER_CH:
+                value, i = _get_number(data, i)
+                rc.append(value)
+            elif ch == ',':
+                i += 1
+            elif ch == ']':
+                i += 1
+                break
+            else:
+                raise Exception("Array, unexpected character %s, pos= %d, "
+                                "data= %s" % (ch, i, data[i:i + 100]))
+    return rc, i
+
+
+def _get_object(data, i, nested=False):
+
+    rc = {}
+
+    identifier = ''
+    state = "U"
+
+    if data is None or len(data) == 0:
+        return {}, 0
+
+    try:
+        while True:
+            ch = data[i:i + 1]
+
+            if ch in _WHITESPACE_CH:
+                i = _ws(data, i)
+            elif ch == '#':
+                i = _comment(data, i)
+            else:
+                if state == "U":
+                    if ch in _IDENT_CH:
+                        (identifier, i) = _get_identifier(data, i)
+                        state = "A"
+                    elif nested and ch == '}':
+                        i += 1
+                        break
+                    else:
+                        raise Exception(
+                            "Unexpected character %s, pos= %d, "
+                            "state= %s, data= %s" %
+                            (ch, i, state, data[i:i + 100]))
+                elif state == "A":
+                    # Look for assignment operator or {
+                    if ch == '=':
+                        # Simple data type
+                        state = 'S'
+                        i += 1
+                    elif ch == '{':
+                        # Section
+                        i += 1
+                        rc[identifier], i = \
+                            _get_object(data, i, True)
+                        state = 'U'
+                    else:
+                        raise Exception(
+                            "Unexpected character %s, pos= %d, "
+                            "state= %s, data= %s" %
+                            (ch, i, state, data[i:i + 100]))
+
+                elif state == "S":
+                    # We are dealing with a number, array or string
+                    if ch == '[':
+                        rc[identifier], i = _get_array(data, i)
+                        state = 'U'
+                    elif ch == '"':
+                        rc[identifier], i = _get_string(data, i)
+                        state = 'U'
+                    elif ch in string.digits:
+                        # Does the format support negative numbers, doesn't
+                        # appear too based on `man 5 lvm.conf`
+                        rc[identifier], i = _get_number(data, i)
+                        state = 'U'
+                    else:
+                        raise Exception(
+                            "Unexpected character %s, pos= %d, "
+                            "state= %s, data= %s" %
+                            (ch, i, state, data[i:i + 100]))
+                else:
+                    raise Exception("Unexpected state %s, pos= %d, "
+                                        "state= %s, data= %s" %
+                                        (ch, i, state, data[i:i + 100]))
+
+    except IndexError:
+        if state != "U":
+            raise Exception("Malformed input, expecting more...")
+    return rc, i
+
+
 class Lvmetad(object):
 
     SOCKET = '/run/lvm/lvmetad.socket'
-
-    IDENT_CH = string.ascii_letters + string.digits + '_.-'
-    WHITESPACE_CH = ' \t\n\r'
-    NUMBER_CH = string.digits + '.'
     END = '''\n##\n'''
 
     def __init__(self):
@@ -46,176 +215,8 @@ class Lvmetad(object):
         self.close()
 
     @staticmethod
-    def _ws(data, i):
-        while data[i] in Lvmetad.WHITESPACE_CH:
-            i += 1
-        return i
-
-    @staticmethod
-    def _comment(data, i):
-        if data[i] != '#':
-            raise Exception(
-                """Comment expecting'#' not %s, pos= %d, data= %s""" %
-                (data[i], i, data[i:i + 100]))
-
-        while data[i] != "\n":
-            i += 1
-        return i + 1        #Chomp new '\n'
-
-    @staticmethod
-    def _get_identifier(data, i):
-        st = i
-        while data[i] in Lvmetad.IDENT_CH:
-            i += 1
-        return data[st:i], i
-
-    @staticmethod
-    def _get_number(data, i):
-        rc = None
-        st = i
-
-        while data[i] in Lvmetad.NUMBER_CH:
-            i += 1
-
-        raw = data[st:i]
-
-        if '.' in raw:
-            rc = float(raw)
-        else:
-            rc = long(raw)
-
-        return rc, i
-
-    @staticmethod
-    def _get_string(data, i):
-
-        if data[i] != '"':
-            raise Exception(
-                """String expecting '"' not %s, pos= %d, data= %s""" %
-                (data[i], i, data[i:i + 100]))
-        i += 1
-        st = i
-
-        while data[i] != '"':
-            i += 1
-        return data[st:i], i + 1            # Chomp the ending '"'
-
-    @staticmethod
-    def _get_array(data, i):
-        rc = []
-
-        if data[i] != '[':
-            raise Exception("Array expecting '[' not %s, pos= %d, data= %s" %
-                            (data[i], i, data[i:i + 100]))
-
-        i += 1
-
-        while True:
-            ch = data[i]
-
-            if ch in Lvmetad.WHITESPACE_CH:
-                i = Lvmetad._ws(data, i)
-            elif ch == '#':
-                i = Lvmetad._comment(data, i)
-            else:
-                # We should only be processing, numbers or strings
-                if ch == '"':
-                    value, i = Lvmetad._get_string(data, i)
-                    rc.append(value)
-                elif ch in Lvmetad.NUMBER_CH:
-                    value, i = Lvmetad._get_number(data, i)
-                    rc.append(value)
-                elif ch == ',':
-                    i += 1
-                elif ch == ']':
-                    i += 1
-                    break
-                else:
-                    raise Exception("Array, unexpected character %s, pos= %d, "
-                                    "data= %s" % (ch, i, data[i:i + 100]))
-        return rc, i
-
-    @staticmethod
-    def _get_object(data, i, nested=False):
-
-        rc = {}
-
-        identifier = ''
-        state = "U"
-
-        if data is None or len(data) == 0:
-            return {}, 0
-
-        try:
-            while True:
-                ch = data[i:i + 1]
-
-                if ch in Lvmetad.WHITESPACE_CH:
-                    i = Lvmetad._ws(data, i)
-                elif ch == '#':
-                    i = Lvmetad._comment(data, i)
-                else:
-                    if state == "U":
-                        if ch in Lvmetad.IDENT_CH:
-                            (identifier, i) = Lvmetad._get_identifier(data, i)
-                            state = "A"
-                        elif nested and ch == '}':
-                            i += 1
-                            break
-                        else:
-                            raise Exception(
-                                "Unexpected character %s, pos= %d, "
-                                "state= %s, data= %s" %
-                                (ch, i, state, data[i:i + 100]))
-                    elif state == "A":
-                        # Look for assignment operator or {
-                        if ch == '=':
-                            # Simple data type
-                            state = 'S'
-                            i += 1
-                        elif ch == '{':
-                            # Section
-                            i += 1
-                            rc[identifier], i = \
-                                Lvmetad._get_object(data, i, True)
-                            state = 'U'
-                        else:
-                            raise Exception(
-                                "Unexpected character %s, pos= %d, "
-                                "state= %s, data= %s" %
-                                (ch, i, state, data[i:i + 100]))
-
-                    elif state == "S":
-                        # We are dealing with a number, array or string
-                        if ch == '[':
-                            rc[identifier], i = Lvmetad._get_array(data, i)
-                            state = 'U'
-                        elif ch == '"':
-                            rc[identifier], i = Lvmetad._get_string(data, i)
-                            state = 'U'
-                        elif ch in string.digits:
-                            # Does the format support negative numbers, doesn't
-                            # appear too based on `man 5 lvm.conf`
-                            rc[identifier], i = Lvmetad._get_number(data, i)
-                            state = 'U'
-                        else:
-                            raise Exception(
-                                "Unexpected character %s, pos= %d, "
-                                "state= %s, data= %s" %
-                                (ch, i, state, data[i:i + 100]))
-                    else:
-                        raise Exception("Unexpected state %s, pos= %d, "
-                                            "state= %s, data= %s" %
-                                            (ch, i, state, data[i:i + 100]))
-
-        except IndexError:
-            if state != "U":
-                raise Exception("Malformed input, expecting more...")
-        return rc, i
-
-    @staticmethod
     def parse(data):
-        return Lvmetad._get_object(data, 0)[0]
+        return _get_object(data, 0)[0]
 
     def _request(self, cmd, args=None):
         req = '''request = "%s"\n''' % cmd
@@ -243,4 +244,4 @@ class Lvmetad(object):
 if __name__ == '__main__':
     with Lvmetad() as meta:
         everything = meta.all()
-        print str(everything)
+        #print str(everything)
