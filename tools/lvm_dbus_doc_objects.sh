5af2b4854e5998aee11719dde3418e472d7abc30
Tony Asleson <tasleson@redhat.com>
2017-02-23 15:06:19 -0600

    Script to create object for docs
    We are using the introspection to create the `api.md` file.  However,
    the objects need to exist before we can document them all!  This script
    tried to create at least 1 object of each possible lvm dbus object type.
    
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

---
+++ tools/lvm_dbus_doc_objects.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Create the needed lvm objects so we can then use the dbus service and
+# introspection data to create the API documentation.
+
+function _g
+{
+    echo "executing: $@"
+    eval "$@"
+    local rc=$?
+    if [ $rc -ne 0 ]; then
+        # We don't do clean on error in case we need to investigate.
+       	vgremove -f doc_vg
+        exit 1
+    fi
+}
+
+# Create a VG
+_g vgcreate doc_vg /dev/sdb /dev/sdc /dev/sdd /dev/sde
+
+# Create a plain linear LV
+_g lvcreate -L4m doc_vg
+
+# Create an old style snapshot
+_g lvcreate --size 4m --snapshot --name old_snap doc_vg/lvol0
+
+# Create a thin pool
+_g lvcreate -T -L512M --name thin_pool doc_vg
+
+# Create a Cache pool
+_g lvcreate -L8m -n lv_cache_meta doc_vg
+_g lvcreate -L16m -n lv_cache doc_vg
+_g lvconvert -y --type cache-pool --poolmetadata doc_vg/lv_cache_meta doc_vg/lv_cache
+  
+# Create another Cache pool
+_g lvcreate -L8M -n lv_cache_meta doc_vg
+_g lvcreate -L16m -n lv_cache_too  doc_vg
+_g lvconvert -y --type cache-pool --poolmetadata doc_vg/lv_cache_meta doc_vg/lv_cache_too
+
+# Create a thin LV
+_g lvcreate -V 16M -T doc_vg/thin_pool -n thin_lv
+
+# Snapshot the thin LV
+_g lvcreate --snapshot --name thin_lv_snap doc_vg/thin_lv
+ 
+
+# Create some LV to cache from cache pool
+_g lvcreate -L128M -n lv_to_cache doc_vg
+
+# Cache the LV
+_g lvconvert -y --type cache --cachepool doc_vg/lv_cache doc_vg/lv_to_cache
+
+echo "Create a job using the dbus interface and then run ./doc_gen.py > api.md"
+ 
+# NOTE: Need to generate a job to have a job object, at the moment just
+# create a simple linear LV in d-feet
+
+# When ready
+# ./doc_gen.py > api.md
+
